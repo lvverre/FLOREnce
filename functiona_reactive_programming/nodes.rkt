347cad583884859593844f882dce80c6937e086b
lvverre <61283085+lvverre@users.noreply.github.com>
2022-02-27 14:16:00 +0100

    changes

--- functiona_reactive_programming/nodes.rkt
+++ functiona_reactive_programming/nodes.rkt
@@ -4,24 +4,24 @@
 (define counter 0)
 
 
-(struct node (predecessors successors visited value)  #:mutable)
+(struct node (test predecessors visited value)  #:mutable)
 
 ;;EVENT
-(struct event-node node (order test)  #:mutable)
+(struct event-node node (order)  #:mutable)
 
 (define (make-event)
   (display "Made event: ")
   (set! counter (+ counter 1))
   (display counter)
   (newline)
-  (let ((new-event (event-node '() '() #f 'undefined '() counter)))
+  (let ((new-event (event-node counter '() #f 'undefined '() )))
     (set-event-node-order! new-event (list new-event))
     new-event))
 
 
 ;FUNCTION
 
-(struct function-node node (function test) )
+(struct function-node node (function) )
 
 (define (make-function-node predecessors function)
    (display "Made function node: ")
@@ -29,22 +29,22 @@
     (set! counter (+ counter 1))
     (display counter)
   (newline)
-  (function-node predecessors '() #f 'undefined  function counter))
+  (function-node counter predecessors  #f 'undefined  function ))
 
 ;OR-node
 
-(struct or-node node (test) )
+(struct or-node node () )
 
 (define (make-or-node left right)
   (display "Made or node: ")
   (set! counter (+ counter 1))
   (display counter)
   (newline)
-  (or-node (list left right) '() #f 'undefined #f counter))
+  (or-node counter (list left right) #f 'undefined  ))
 
 ;FILTER-NODE
 
-(struct filter-node node ( filter test) )
+(struct filter-node node ( filter ) )
 
 (define (make-filter-node predecessors filter )
    (display "Made filter event: ")
@@ -52,9 +52,9 @@
     (set! counter (+ counter 1))
     (display counter)
   (newline)
-  (filter-node predecessors '() #f 'undefined  filter counter))
+  (filter-node counter predecessors  #f 'undefined  filter ))
 
-(struct wrapper-or-node (node activation))
+
 
          
          
@@ -70,9 +70,15 @@
     (set! counter (+ counter 1))
     (display counter)
   (newline)
-  (let ((new-event (event-node predecessors '() #f 'undefined '() counter)))
+  (let ((new-event (event-node counter predecessors  #f 'undefined '() )))
     (set-event-node-order! new-event (list new-event))
     new-event))
+
+
+;;JUMP node
+(struct jump-node node (to-node condition))
+(define (make-jump-node to-node condition)
+  (jump-node 
 
 
 
