bcafb610b1d0b24922e2384679a28538781bffb6
lvverre <61283085+lvverre@users.noreply.github.com>
2022-02-28 14:25:27 +0100

    remove-observer + or event for topologic sorting

--- functiona_reactive_programming/nodes.rkt
+++ functiona_reactive_programming/nodes.rkt
@@ -4,47 +4,47 @@
 (define counter 0)
 
 
-(struct node (predecessors successors visited value)  #:mutable)
+(struct node (test predecessors visited value)  #:mutable)
 
 ;;EVENT
-(struct event-node node (order test)  #:mutable)
+(struct event-node node (order leafs n-successors)  #:mutable)
 
 (define (make-event)
   (display "Made event: ")
   (set! counter (+ counter 1))
   (display counter)
   (newline)
-  (let ((new-event (event-node '() '() #f 'undefined '() counter)))
+  (let ((new-event (event-node counter '() #f 'undefined '() '() 0)))
     (set-event-node-order! new-event (list new-event))
     new-event))
 
 
 ;FUNCTION
 
-(struct function-node node (function test) )
+(struct function-node node (function ) )
 
-(define (make-function-node predecessors function)
+(define (make-function-node predecessors function )
    (display "Made function node: ")
 
     (set! counter (+ counter 1))
     (display counter)
   (newline)
-  (function-node predecessors '() #f 'undefined  function counter))
+  (function-node counter predecessors #f 'undefined  function ))
 
 ;OR-node
 
-(struct or-node node (test) )
+(struct or-node node () )
 
-(define (make-or-node left right)
+(define (make-or-node left right )
   (display "Made or node: ")
   (set! counter (+ counter 1))
   (display counter)
   (newline)
-  (or-node (list left right) '() #f 'undefined #f counter))
+  (or-node counter (list left right) #f 'undefined ))
 
 ;FILTER-NODE
 
-(struct filter-node node ( filter test) )
+(struct filter-node node ( filter ) #:mutable )
 
 (define (make-filter-node predecessors filter )
    (display "Made filter event: ")
@@ -52,27 +52,32 @@
     (set! counter (+ counter 1))
     (display counter)
   (newline)
-  (filter-node predecessors '() #f 'undefined  filter counter))
-
-(struct wrapper-or-node (node activation))
-
-         
-         
-
-
-
-
+  (filter-node counter predecessors #f 'undefined  filter ))
   
 ;;MAP EVENT
-(define (make-map-event predecessors)
+(define (make-event-with-predecessor predecessors )
   (display "Made map event: ")
 
     (set! counter (+ counter 1))
     (display counter)
   (newline)
-  (let ((new-event (event-node predecessors '() #f 'undefined '() counter)))
+  (let ((new-event (event-node counter predecessors #f  'undefined '() '() 0)))
     (set-event-node-order! new-event (list new-event))
     new-event))
+
+;;MAKE-JUMP-NODE
+(struct start-jump-node (counter condition jump-to) #:mutable)
+(define (make-start-jump-node condition jump-to )
+  (set! counter (+ counter 1))
+
+  (start-jump-node counter condition jump-to))
+
+(struct end-jump-node (counter direction))
+(define (make-end-jump-node direction)
+  (set! counter (+ counter 1))
+
+  (end-jump-node counter direction))
+  
 
 
 
